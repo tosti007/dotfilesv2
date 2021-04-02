3a8289272047d7e8ee95f051d0b79155a688eec3
tosti007 <brian-janssen@hotmail.com>
2021-04-02 13:15:04 +0200

    Fish: update aliases

--- user/fish/.config/fish/conf.d/aliases.fish
+++ user/fish/.config/fish/conf.d/aliases.fish
@@ -1,6 +1,8 @@
 # Command overwrites and shortcuts
 alias rmr "rm -rf"
 alias xclip "xclip -selection clipboard"
+alias rgpdf "rg --pre $HOME/.dotfiles/assets/rgpdf-pre.sh --pre-glob '*.pdf'"
+alias v "vscodium ."
 
 # ls shortcuts, but with exa because that's cooler
 alias ls "exa --group-directories-first"
@@ -11,7 +13,6 @@ alias tree "ls --tree"
 # cat shortcuts, but with bat
 alias cat "bat"
 alias ccat "bat --plain --color never --pager never"
-alias rgpdf "rg --pre $HOME/.dotfiles/assets/rgpdf-pre.sh --pre-glob '*.pdf'"
 
 # git aliases
 alias gis "git status"
