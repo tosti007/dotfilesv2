5956265929e60ee11cd5750417d6e19cc5d6a4a0
tosti007 <brian-janssen@hotmail.com>
2021-07-06 15:53:38 +0200

    Sway: Initial Commit

---
+++ user/sway/.config/ruwal/hooks/sway/convert.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+cd ~/.cache/ruwal/sway
+magick convert -background none borders.svg borders.png
---
+++ user/sway/.config/sway/scripts/sunset.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTENT=$(curl -s https://freegeoip.app/json/)
+longitude=$(echo $CONTENT | jq .longitude)
+latitude=$(echo $CONTENT | jq .latitude)
+
+wlsunset -l $latitude -L $longitude
